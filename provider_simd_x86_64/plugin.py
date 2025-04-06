88bca122491995ea59af8ca2e0f2dddd97eac7bb
Jonathan Dekhtiar <jonathan@dekhtiar.com>
2025-04-06 16:18:35 -0400

    API Update

--- provider_simd_x86_64/plugin.py
+++ provider_simd_x86_64/plugin.py
@@ -1,17 +1,55 @@
+from __future__ import annotations
+
 import os
 
-from variantlib.config import KeyConfig
-from variantlib.config import ProviderConfig
-
 from provider_simd_x86_64 import __version__
+from variantlib.models.provider import ProviderConfig
+from variantlib.models.provider import VariantFeatureConfig
 
-FLAGS = """
-3dnow 3dnowext aes avx avx2 avx512_4fmaps avx512_4vnniw avx512_bf16
-avx512_bitalg avx512_fp16 avx512_vbmi2 avx512_vnni avx512_vp2intersect
-avx512_vpopcntdq avx512bw avx512cd avx512dq avx512er avx512f avx512ifma
-avx512pf avx512vbmi avx512vl f16c fma3 fma4 mmx mmxext padlock pclmul popcnt
-rdrand sha sse sse2 sse3 sse4_1 sse4_2 sse4a ssse3 vpclmulqdq xop
-""".split()
+FLAGS = [
+    "3dnow",
+    "3dnowext",
+    "aes",
+    "avx",
+    "avx2",
+    "avx512_4fmaps",
+    "avx512_4vnniw",
+    "avx512_bf16",
+    "avx512_bitalg",
+    "avx512_fp16",
+    "avx512_vbmi2",
+    "avx512_vnni",
+    "avx512_vp2intersect",
+    "avx512_vpopcntdq",
+    "avx512bw",
+    "avx512cd",
+    "avx512dq",
+    "avx512er",
+    "avx512f",
+    "avx512ifma",
+    "avx512pf",
+    "avx512vbmi",
+    "avx512vl",
+    "f16c",
+    "fma3",
+    "fma4",
+    "mmx",
+    "mmxext",
+    "padlock",
+    "pclmul",
+    "popcnt",
+    "rdrand",
+    "sha",
+    "sse",
+    "sse2",
+    "sse3",
+    "sse4_1",
+    "sse4_2",
+    "sse4a",
+    "ssse3",
+    "vpclmulqdq",
+    "xop",
+]
 
 
 class SIMD_X86_64_Plugin:  # noqa: N801
@@ -24,7 +62,7 @@ class SIMD_X86_64_Plugin:  # noqa: N801
         for i, simd in enumerate(FLAGS):
             if i == stop_after:
                 break
-            yield KeyConfig(key=simd, values=["1"])
+            yield VariantFeatureConfig(name=simd, values=["1"])
 
     def get_supported_configs(self) -> ProviderConfig:
         keyconfigs = list(self._get_capabilities())
